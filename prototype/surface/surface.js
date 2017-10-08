457e9b5c7ee10e9908307563ccd647c1cdac1d30
CALICAWESOME <greendolphinavenue@gmail.com>
2017-10-08 14:15:48 -0400

    make some more better listeners

--- prototype/surface/surface.js
+++ prototype/surface/surface.js
@@ -2,6 +2,9 @@
 // 2017
 
 const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+
+// TODO: turn this into a class/module once we know it works
 
 const baudRate = 696969; // TODO: Chris what the heck baud rate did you use I forget
 
@@ -14,7 +17,7 @@ SerialPort.list()
         for (let i = 0; i < ports.length; i++) {
             // if the port's manufacturer contains "Arduino"
             if (ports[i].manufacturer.indexOf('Arduino') !== -1)
-            // return a new SerialPort object from that port
+                // return a new SerialPort object opened on that port
                 port = new SerialPort(ports[i].comName, {
                     baudRate: baudRate
                 });
@@ -22,7 +25,16 @@ SerialPort.list()
         throw 'Arduino not found';
     })
     .then(() => {
-        port.on('data', () => {
-            console.log('Data:', port.read());
+        const parser = new Readline();
+        parser.pipe(port);
+        // set up our listeners
+        port.on('open', () => {
+            console.log('Serial port opened!');
+        });
+        port.on('close', () => {
+            console.log('Serial port closed');
+        });
+        parser.on('data', (data) => {
+            console.log('Data: ', data)
         });
     });
