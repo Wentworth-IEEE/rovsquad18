d1eb0240b9b6e89fde274603a45e016c66e3e50b
CALICAWESOME <greendolphinavenue@gmail.com>
2017-10-12 21:44:00 -0400

    make promise chain actually work for realsies
    - also fix baud rate

--- prototype/surface/surface.js
+++ prototype/surface/surface.js
@@ -4,37 +4,29 @@
 const SerialPort = require('serialport');
 const Readline = SerialPort.parsers.Readline;
 
-// TODO: turn this into a class/module once we know it works
+// TODO: turn this into a module once we know it works
 
-const baudRate = 696969; // TODO: Chris what the heck baud rate did you use I forget
+const baudRate = 19200;
 
-let port;
 // this works in theory but has not been tested
-SerialPort.list()
-    .then((ports) => {
-        console.log(ports);
-        // look through each COM port returned by SerialPort.list()
-        for (let i = 0; i < ports.length; i++) {
-            // if the port's manufacturer contains "Arduino"
-            if (ports[i].manufacturer.indexOf('Arduino') !== -1)
-                // return a new SerialPort object opened on that port
-                port = new SerialPort(ports[i].comName, {
-                    baudRate: baudRate
-                });
-        }
-        throw 'Arduino not found';
-    })
-    .then(() => {
-        const parser = new Readline();
-        parser.pipe(port);
-        // set up our listeners
-        port.on('open', () => {
-            console.log('Serial port opened!');
-        });
-        port.on('close', () => {
-            console.log('Serial port closed');
-        });
-        parser.on('data', (data) => {
-            console.log('Data: ', data)
-        });
+SerialPort.list().then((ports) => {
+    // look through each COM port returned by SerialPort.list()
+    for (let i = 0; i < ports.length; i++) {
+        // if the port's manufacturer contains "Arduino"
+        if (ports[i].manufacturer.indexOf('Arduino') !== -1)
+        // return a new SerialPort object opened on that port
+            return new SerialPort(ports[i].comName, {
+                baudRate: baudRate
+            });
+    }
+    // throw an error if we don't find the arduino
+    throw 'Arduino not found';
+}).then((port) => {
+    const parser = port.pipe(new Readline());
+    port.on('open', () => {
+        console.log('HEYYY');
     });
+    parser.on('data', (data) => {
+        console.log(data);
+    });
+});
