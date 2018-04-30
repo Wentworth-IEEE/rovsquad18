0fba79941543154fb47c9bf026f475bdd2aa6c32
Chris Thierauf <chris@cthierauf.com>
2018-04-30 17:17:15 -0400

    Claw stuff

--- js/packages/test-sequence/claw-test.js
+++ js/packages/test-sequence/claw-test.js
@@ -1,13 +1,13 @@
 const i2cbus = require('i2c-bus');
 const { Pca9685Driver } = require("pca9685");
 const {nugLog, levels} = require('../nugget-logger');
-const logger = new nugLog('verbose', 'remote.log');
-console.log('Logging!');
+const logger = new nugLog('verbose', 'clawTest.log');
+console.log('Logging! (Running claw tests, log in "clawTest.log")');aaaaa
 
 const pca = new Pca9685Driver({
     i2c: i2cbus.openSync(1),
     address: 0x40,
-    frequency: 50,
+    frequency: 300,
     debug: false
 }, error => {
     if (error) {
@@ -15,23 +15,25 @@ const pca = new Pca9685Driver({
         throw error;
     }
     logger.i('PCA Init', 'PCA up!');
+
+    
+    let dutyCycle = .55
+    for(let i=0; i < 7; i++) {
+        setInterval(() => {
+            pca.setDutyCycle(0, dutyCycle);
+        }, 250);
+        if(i === 0 || i === 2 || i === 4 || i === 6) {
+            dutyCycle = .5;
+        } else if(i === 1) {
+            dutyCycle = .55;
+        } else if(i === 3 || i === 5) {
+            dutyCycle = .45;
+        }
+    }
+        
+    
 });
 
 // Test the claw by opening a bit, pausing, opening a bit more, pausing,
 // closing a bit, pausing, closing a bit more.
-
-let dutyCycle = .55
-for(let i=0; i < 7; i++) {
-    setInterval(() => {
-        pca.setDutyCycle(0, dutyCycle);
-    }, 250);
-    if(i === 0 || i === 2 || i === 4 || i === 6) {
-        dutyCycle = .5;
-    } else if(i === 1) {
-        dutyCycle = .55;
-    } else if(i === 3 || i === 5) {
-        dutyCycle = .45;
-    }
-}
-    
 
