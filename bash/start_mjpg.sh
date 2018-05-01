acd859d60b1e266a07e849fc9e361f503df3d160
Chris Thierauf <chris@cthierauf.com>
2018-05-01 19:19:38 -0400

    Added Pi cam stuff

--- bash/start_mjpg.sh
+++ bash/start_mjpg.sh
@@ -2,5 +2,6 @@
 # Starts the MJPG_stream stuff- 
 # A stream will appear on 8081, using the best settings I could figure for low latency/stalling.
 mjpg_streamer -i 'input_uvc.so -softfps 15 -d /dev/video0 -q 100' -o 'output_http.so -p 8081'
-# TODO- do the same with the Pi camera. Not working atm for some reason
+# This one does the Pi
+mjpg_streamer -i 'input_raspicam.so -q 25 -fps 25' -o 'output_http.so -p 8082'
 
